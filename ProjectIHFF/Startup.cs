9dd799d84c032636f7db73a3152960449e3ba240
ivotjebot <game4life49@live.nl>
2016-12-05 15:05:22 +0100

    fdasfadsf

---
+++ ProjectIHFF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectIHFF.Startup))]
+namespace ProjectIHFF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
