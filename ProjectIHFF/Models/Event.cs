fb0b49a7858d7f1f1a31871a18099788531c468f
SchopmanBente <b.l.schopman@gmail.com>
2016-12-07 13:03:02 +0100

    Event gevuld

--- ProjectIHFF/Models/Event.cs
+++ ProjectIHFF/Models/Event.cs
@@ -7,5 +7,36 @@ namespace ProjectIHFF.Models
 {
     class Event
     {
+        public int id;
+        public float prijs;
+
+        public string Naam { get; set; }
+        public DateTime beginDatumTijd { get; set; }
+        public DateTime eindDatumTijd { get; set; }
+        public Locatie Locatie { get; set; }
+
+        public Event()
+        {
+
+        }
+
+        public Event(int id, float prijs, string naam, DateTime beginDatum, DateTime eindDatum, Locatie locatie)
+        {
+            this.id = id;
+            this.prijs = prijs;
+            this.Naam = naam;
+            this.beginDatumTijd = beginDatum;
+            this.eindDatumTijd = eindDatum;
+            this.Locatie = locatie;
+        }
+
+        public Event(float prijs, string naam, DateTime beginDatum, DateTime eindDatum, Locatie locatie)
+        {
+            this.prijs = prijs;
+            this.Naam = naam;
+            this.beginDatumTijd = beginDatum;
+            this.eindDatumTijd = eindDatum;
+            this.Locatie = locatie;
+        }
     }
 }
--- ProjectIHFF/Models/Film.cs
+++ ProjectIHFF/Models/Film.cs
@@ -5,7 +5,20 @@ using System.Text;
 
 namespace ProjectIHFF.Models
 {
-    class Film
+    class Film : Event
     {
+        public string Beschrijving { get; set; }
+        public string TrailerURL { get; set; }
+        public string PosterURL { get; set; }
+        public float Rating { get; set; }
+
+        public Film(string beschrijving, string trailer, string poster, float rating, float prijs, string naam, DateTime beginDatum, DateTime eindDatum, Locatie locatie)
+            : base(prijs, naam, beginDatum, eindDatum, locatie)
+        {
+            this.Beschrijving = beschrijving;
+            this.TrailerURL = trailer;
+            this.PosterURL = poster;
+            this.Rating = rating;
+        }
     }
 }
