1b7af27aa8cf635e937704a8369bb9e4dc48ef08
SchopmanBente <b.l.schopman@gmail.com>
2016-12-07 13:22:59 +0100

    Special verbeterd

--- ProjectIHFF/Models/Special.cs
+++ ProjectIHFF/Models/Special.cs
@@ -8,7 +8,6 @@ namespace ProjectIHFF.Models
     class Special : Event
     {
         public string Beschrijving { get; set; }
-        public Event Gebeurtenis { get; set; }
         public string Naam { get; set; }
         public string Spreker { get; set; }
 
@@ -19,20 +18,18 @@ namespace ProjectIHFF.Models
 
         }
 
-        public Special(string beschrijving, Locatie locatie,Event gebeurtenis, string naam, string spreker,float prijs, DateTime beginDatum, DateTime eindDatum)
+        public Special(string beschrijving, Locatie locatie, string naam, string spreker,float prijs, DateTime beginDatum, DateTime eindDatum)
             :base(prijs,naam,beginDatum,eindDatum,locatie)
         {
             this.Beschrijving = beschrijving;
-            this.Gebeurtenis  = gebeurtenis;
             this.Naam = naam;
             this.Spreker = spreker;
         }
 
-        public Special(int id, string beschrijving, Locatie locatie, Event gebeurtenis, string naam, string spreker, float prijs, DateTime beginDatum, DateTime eindDatum)
+        public Special(int id, string beschrijving, Locatie locatie, string naam, string spreker, float prijs, DateTime beginDatum, DateTime eindDatum)
             : base(id,prijs, naam, beginDatum, eindDatum, locatie)
         {
             this.Beschrijving = beschrijving;
-            this.Gebeurtenis = gebeurtenis;
             this.Naam = naam;
             this.Spreker = spreker;
         }
