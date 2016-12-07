eb70c3ebe03fa718862aba2a2289c7e6b9c895d7
MathijsvanHeerden <567604@student.inholland.nl>
2016-12-07 14:08:11 +0100

    Alle models gevuld met properties/fields

--- ProjectIHFF/Models/CultuurItem.cs
+++ ProjectIHFF/Models/CultuurItem.cs
@@ -5,7 +5,35 @@ using System.Text;
 
 namespace ProjectIHFF.Models
 {
- public   class CultuurItem
+    public class CultuurItem
     {
+        public int id;
+        public string naam { get; set; }
+        public Locatie locatie { get; set; }
+        public DateTime openingsstarttijd { get; set; }
+        public DateTime openingseindtijd { get; set; }
+
+        public CultuurItem()
+        {
+
+        }
+
+        public CultuurItem(int id, string naam, Locatie locatie, DateTime openingsstarttijd, DateTime openingseindtijd)
+        {
+            this.id = id;
+            this.naam = naam;
+            this.locatie = locatie;
+            this.openingsstarttijd = openingsstarttijd;
+            this.openingseindtijd = openingseindtijd; 
+        }
+
+        public CultuurItem(string naam, Locatie locatie, DateTime openingsstarttijd, DateTime openingseindtijd)
+        {
+            
+            this.naam = naam;
+            this.locatie = locatie;
+            this.openingsstarttijd = openingsstarttijd;
+            this.openingseindtijd = openingseindtijd;
+        }
     }
 }
--- ProjectIHFF/Models/Reservering.cs
+++ ProjectIHFF/Models/Reservering.cs
@@ -7,5 +7,35 @@ namespace ProjectIHFF.Models
 {
     public class Reservering
     {
+       public int id;
+       public int klantid;
+        public bool is_betaald { get; set;}
+        public bool is_geannuleerd { get; set; }
+        public DateTime besteldatum { get; set; }
+        public string ophaalcode { get; set; }
+
+        public Reservering()
+        {
+
+        }
+
+        public Reservering(int id, int klantid, bool is_betaald, bool is_geannuleerd, DateTime besteldatum, string ophaalcode)
+        {
+            this.id = id;
+            this.klantid = klantid;
+            this.is_betaald = is_betaald;
+            this.is_geannuleerd = is_geannuleerd;
+            this.besteldatum = besteldatum;
+            this.ophaalcode = ophaalcode; 
+        }
+
+        public Reservering( bool is_betaald, bool is_geannuleerd, DateTime besteldatum, string ophaalcode)
+        {
+            
+            this.is_betaald = is_betaald;
+            this.is_geannuleerd = is_geannuleerd;
+            this.besteldatum = besteldatum;
+            this.ophaalcode = ophaalcode;
+        }
     }
 }
