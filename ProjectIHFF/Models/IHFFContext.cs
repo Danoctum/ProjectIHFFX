ffb3aaace84b3e5c6655fdf9ed5386045992a37a
SchopmanBente <b.l.schopman@gmail.com>
2016-12-07 12:43:59 +0100

    Domainmodels aangemaakt + DbContext

---
+++ ProjectIHFF/Models/IHFFContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectIHFF.Models
+{
+    public class IHFFContext : DbContext
+    {
+        public IHFFContext()
+            : base("IHFFConnection")
+        {
+
+        }
+
+        public DbSet<Reservering> Reservings { get; set; }
+        public DbSet<Klant> Klants { get; set; }
+        public DbSet<Klant_Reservering> klant_reserverings { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Maaltijd> Maaltijds { get; set; }
+        public DbSet<Restaurant> Restaurant { get; set; }
+        public DbSet<Locatie> Locaties { get; set; }
+        public DbSet<Special> Specials { get; set; }
+        public DbSet<Voorstelling> Voorstellings { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<CultuurItem> CultuurItems { get; set; }
+ 
+
+    }
+}
